716947611f55f83baab0e9705201413f1631efe2
kadir4554 <kadiregriboyun98@gmail.com>
2022-04-21 15:52:36 +0300

    Add project files.

---
+++ BlogProject.Data/Concrete/EntityFramework/Repositories/EfCommentRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlogProject.Data.Abstract;
+using BlogProject.Entities.Concrete;
+using BlogProject.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfCommentRepository:EfEntityRepositoryBase<Comment>,ICommentRepository
+    {
+        public EfCommentRepository(DbContext context):base(context)
+        {
+
+        }
+    }
+}
