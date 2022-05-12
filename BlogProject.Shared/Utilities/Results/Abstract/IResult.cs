467233886ee3d2b7260808cec14637acc0a04d15
kadir4554 <kadiregriboyun98@gmail.com>
2022-05-12 19:53:24 +0300

    Result yapımızı oluşturduk.

---
+++ BlogProject.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,17 @@
+﻿using BlogProject.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlogProject.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; set; }//ResultStatus.Success or ResultStatus.Error
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
---
+++ BlogProject.Shared/Utilities/Results/ComplexTypes/ResultStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Shared.Utilities.Results.ComplexTypes
+{
+    public enum ResultStatus
+    {
+        Success=0,//ResultStatus.Success
+        Error =1, //ResultStatus.Error
+        Warning =2,//ResultStatus.Warning
+        Info=3,//ResultStatus.Info
+    }
+}
